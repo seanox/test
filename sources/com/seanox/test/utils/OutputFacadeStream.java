59bc3675bb9aa167cd05f5398a1fa824679d7315
seanox <service@seanox.de>
2020-04-11 18:36:43 +0200

    #0000 OutputFacadeStream Write: Add a flush #0000 OutputFacadeStream Capture: Added various await methods

--- sources/com/seanox/test/utils/OutputFacadeStream.java
+++ sources/com/seanox/test/utils/OutputFacadeStream.java
@@ -4,7 +4,7 @@
  *  Diese Software unterliegt der Version 2 der GNU General Public License.
  *
  *  Seanox Test SDK
- *  Copyright (C) 2017 Seanox Software Solutions
+ *  Copyright (C) 2020 Seanox Software Solutions
  *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of version 2 of the GNU General Public License as published
@@ -33,12 +33,12 @@ import java.util.HashSet;
  *  output streams. The output streams can be flexibly added and removed at
  *  runtime or created temporarily.<br>
  *  <br>
- *  OutputFacadeStream 1.0 20171231<br>
- *  Copyright (C) 2017 Seanox Software Solutions<br>
+ *  OutputFacadeStream 1.1.0 20200411<br>
+ *  Copyright (C) 2020 Seanox Software Solutions<br>
  *  All rights reserved.
  *
  *  @author  Seanox Software Solutions
- *  @version 1.0 20171231
+ *  @version 1.1.0 20200411
  */
 public class OutputFacadeStream extends OutputStream {
     
@@ -117,8 +117,10 @@ public class OutputFacadeStream extends OutputStream {
         synchronized (OutputFacadeStream.class) {
             streams = this.outputStreams.toArray(new OutputStream[0]);
         }
-        for (OutputStream outputStream : streams)
+        for (OutputStream outputStream : streams) {
             outputStream.write(data, offset, length);
+            outputStream.flush();
+        }
     }
     
     @Override
@@ -149,12 +151,100 @@ public class OutputFacadeStream extends OutputStream {
         /** Constructor, creates a new Capture object. */
         private Capture() {
         }
+
+        /**
+         *  Wait until a line break can be detected.
+         *  Without a line break, the method will block.
+         *  @throws InterruptedException
+         */        
+        public void await()
+                throws InterruptedException {
+            try {this.await(-1);
+            } catch (TimeoutException exception) {
+                return;
+            }            
+        }
         
+        /**
+         *  Wait until a line break can be detected.
+         *  Optionally, a timeout can be specified if the methods should not
+         *  block endlessly.
+         *  @param  timeout
+         *  @throws TimeoutException
+         *      In case when a timeout has been set that has been exceeded.
+         *  @throws InterruptedException
+         */
+        public void await(long timeout)
+                throws TimeoutException, InterruptedException {
+            
+            String content = this.toString();
+            
+            long timing = System.currentTimeMillis();
+            while (true) {
+                if (timeout >= 0
+                        && (System.currentTimeMillis() >= timing +timeout))
+                    throw new TimeoutException();
+                if (content.length() != this.toString().length()
+                        && this.toString().matches("(?s)^.*[\r\n]+$"))
+                    break;
+                Thread.sleep(25);
+            }
+        }
+        
+        /**
+         *  Wait until the pattern can be detected.
+         *  Without the pattern, the method will block.
+         *  @param  pattern
+         *  @throws InterruptedException
+         */          
+        public void await(String pattern)
+                throws InterruptedException {
+            try {this.await(pattern, -1);
+            } catch (TimeoutException exception) {
+                return;
+            }
+        }
+
+        /**
+         *  Wait until a pattern can be detected.
+         *  Optionally, a timeout can be specified if the methods should not
+         *  block endlessly.
+         *  @param  pattern
+         *  @param  timeout
+         *  @throws TimeoutException
+         *      In case when a timeout has been set that has been exceeded.
+         *  @throws InterruptedException
+         */        
+        public void await(String pattern, long timeout)
+                throws TimeoutException, InterruptedException {
+            
+            if (pattern == null
+                    || pattern.trim().isEmpty())
+                throw new IllegalArgumentException("Invalid pattern");
+
+            long timing = System.currentTimeMillis();
+            while (true) {
+                if (timeout >= 0
+                        && (System.currentTimeMillis() >= timing +timeout))
+                    throw new TimeoutException();
+                String content = this.toString();
+                if (content.matches(pattern))
+                    break;
+                Thread.sleep(25);
+            }
+        }
+
         @Override
         public void close() throws IOException {
             
             OutputFacadeStream.this.unmount(this);
             super.close();
+        }
+        
+        /** TimeoutException */
+        public class TimeoutException extends Exception {
+
+            private static final long serialVersionUID = 4355449388335541238L;
         }
     }
 }
